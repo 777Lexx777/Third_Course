29dc02562c707822995426f1c9363de7c0576e46
geekbrains <lex156166@gmail.com>
2020-09-28 17:53:03 +0400

    Test

---
+++ untitled/src/Lesson_1/Main.java
@@ -0,0 +1,44 @@
+package Lesson_1;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        ShuttleContainer container = new ShuttleContainer<>();
+//        container.put(new SpaceCargo());
+//        container.put(new DronCargo());
+//        System.out.println(container);
+//
+//        ShuttleContainer<DronCargo> dronContainer = new ShuttleContainer<>();
+//        dronContainer.put(new DronCargo());
+//        System.out.println(dronContainer);
+
+
+    }
+
+//    static void doDemoGenerics() {
+//        Container<Auto> autoContainer = new CargoContainer<>();
+//        autoContainer.put(new Auto());
+//        autoContainer.put(new Auto());
+//
+//        System.out.println(autoContainer);
+//
+//        Container<Harvester> harvesterContainer = new CargoContainer<>();
+//        harvesterContainer.put(new Harvester());
+//
+//        System.out.println(harvesterContainer);
+//
+//        Container<SpaceCargo> spaceDetailContainer = new FlyCargoContainer<>();
+//        spaceDetailContainer.put(new SpaceCargo());
+//        spaceDetailContainer.put(new SpaceCargo());
+//
+//        System.out.println(spaceDetailContainer);
+//
+//        Container<SpaceCargo> spaceDetailCargoContainer = new CargoContainer<>();
+//        spaceDetailCargoContainer.put(new SpaceCargo());
+//        spaceDetailCargoContainer.put(new SpaceCargo());
+//
+//        System.out.println(spaceDetailCargoContainer);
+//    }
+}
